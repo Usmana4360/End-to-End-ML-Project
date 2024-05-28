e6309d7f21bade7b4c11b56fba543aa2ea94f322
Muhammad Usman <usmanashraf4360@gmail.com>
2024-05-28 13:17:38 +0500

    EDA

---
+++ src/exception.py
@@ -0,0 +1,8 @@
+import sys
+
+def error_message_details(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message='error occured in python script name[{0}] line numner[{1}] error message[{2}]'
+    file_name,exc_tb.tb_lineno,str(error)
+    
